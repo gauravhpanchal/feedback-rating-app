8cb7657b5c60eaa4f22464b86cdba3ccee4bf1ef
Gaurav <gauravhpanchal2@gmail.com>
2024-08-17 21:31:37 +0530

    Final changes

--- src/components/Body.js
+++ src/components/Body.js
@@ -21,7 +21,10 @@ const Body = () => {
       </div>
       <div className="text-center w-48 mx-auto">
         <h1 className="text-4xl text-center font-bold mt-10">Reviews</h1>
-        <div className="flex justify-center gap-1 mt-2" title="Average of all the reviews">
+        <div
+          className="flex justify-center gap-1 mt-2"
+          title="Average of all the reviews"
+        >
           {Array.from({ length: 5 }, (_, i) => (
             <Star key={i} filled={i < roundedRating} />
           ))}
@@ -40,37 +43,43 @@ const Body = () => {
         WRITE A REVIEW
       </Link>
       <div className="mt-8">
-        {reviews.length===0?(<p className="text-xl text-center text-[#cacaca]">No Reviews Yet</p>):(reviews.map((review, index) => (
-          <div key={index} className="mb-4 p-4 border rounded-lg">
-            <div className="flex gap-1 mb-3 items-center">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="size-8"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
-                />
-              </svg>
-              <h3 className="font-semibold text-gray-700 text-base">
-                {review.username}
-              </h3>
-            </div>
+        {reviews.length === 0 ? (
+          <p className="text-xl text-center text-[#cacaca]">No Reviews Yet</p>
+        ) : (
+          reviews.map((review, index) => (
+            <div key={index} className="mb-4 p-4 border rounded-lg">
+              <div className="flex gap-1 mb-3 items-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="size-8"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                  />
+                </svg>
+                <h3 className="font-semibold text-gray-700 text-base">
+                  {review.username}
+                </h3>
+              </div>
 
-            <div className="flex gap-1">
-              {Array.from({ length: 5 }, (_, i) => (
-                <Star key={i} filled={i < review.rating} />
-              ))}
+              <div className="flex gap-1">
+                {Array.from({ length: 5 }, (_, i) => (
+                  <Star key={i} filled={i < review.rating} />
+                ))}
+              </div>
+              <h3 className="font-semibold text-xl mt-4">{review.title}</h3>
+              <p className="font-medium text-lg text-gray-600">
+                {review.review}
+              </p>
             </div>
-            <h3 className="font-semibold text-xl mt-4">{review.title}</h3>
-            <p className="font-medium text-lg text-gray-600">{review.review}</p>
-          </div>
-        )))}
+          ))
+        )}
       </div>
     </div>
   );
